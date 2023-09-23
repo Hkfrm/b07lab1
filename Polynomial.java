698d5d710d58aefb0b44c14bf9c0eaa316dc80c5
Hkfrm <144830997+Hkfrm@users.noreply.github.com>
2023-09-23 17:29:31 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,17 +1,11 @@
 public class Polynomial {
     private double[] coefficients;
-
-    // No-argument constructor that sets the polynomial to zero
     public Polynomial() {
         this.coefficients = new double[]{0};
     }
-
-    // Constructor that takes an array of double as an argument
     public Polynomial(double[] coefficients) {
         this.coefficients = coefficients;
     }
-
-    // Method to add two polynomials
     public Polynomial add(Polynomial other) {
         int maxLength = Math.max(this.coefficients.length, other.coefficients.length);
         double[] resultCoefficients = new double[maxLength];
@@ -26,8 +20,6 @@ public class Polynomial {
 
         return new Polynomial(resultCoefficients);
     }
-
-    // Method to evaluate the polynomial for a given value of x
     public double evaluate(double x) {
         double result = 0;
 
@@ -37,8 +29,6 @@ public class Polynomial {
 
         return result;
     }
-
-    // Method to check if a given value is a root of the polynomial
     public boolean hasRoot(double x) {
         return evaluate(x) == 0;
     }
